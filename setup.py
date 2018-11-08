43b8011141f5176a30586c31b100aefaf466429b
davelush <dave.lush@gmail.com>
2018-11-08 20:25:07 +0000

    Adding a setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="SlackBotSandbox",
+    version="0.0.1-snapshot",
+    packages=['slack-bot',],
+    license='Apache License 2.0',
+    long_description=open('README.md').read(),
+)
