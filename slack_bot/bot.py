56be373ee5b54d0ecd26874435c707d601e13485
davelush <dave.lush@gmail.com>
2018-11-27 20:57:35 +0000

    Cleaned up lots of comments where I've got to grips with how this works

--- slack_bot/bot.py
+++ slack_bot/bot.py
@@ -6,19 +6,25 @@ from slackclient import SlackClient
 # To remember which teams have authorized your app and what tokens are
 # associated with each team, we can store this information in memory on
 # as a global object.
-#FIXME When your bot is out of development, it's best to save to a more persistent memory store.
+# FIXME When your bot is out of development, it's best to save to a more persistent memory store.
 authed_teams = {}
 kudos = {}
 
+
 class Bot(object):
     """ Instantiates a Bot object to handle Slack onboarding interactions."""
+
     def __init__(self, logger, postgres_connection):
         super(Bot, self).__init__()
+
+        # TODO there has to be a better way of getting a logger than passing it from Flask
         self.logger = logger
+
+        # bot identitiy
         self.name = "gonzo"
         self.emoji = ":peanut_butter_jelly_time:"
-        # When we instantiate a new bot object, we can access the app
-        # credentials we set earlier in our local development environment.
+
+        # Security & verification
         self.oauth = {"client_id": os.environ.get("CLIENT_ID"),
                       "client_secret": os.environ.get("CLIENT_SECRET"),
                       "scope": "bot"}
@@ -28,86 +34,31 @@ class Bot(object):
         # an oauth token. We can connect to the client without authenticating
         # by passing an empty string as a token and then reinstantiating the
         # client with a valid OAuth token once we have one.
-        #FIXME this BOT_TOKEN is not the right way to do OAuth
+        # FIXME this BOT_TOKEN is not the right way to do OAuth
         self.bot_token = os.environ.get("BOT_TOKEN")
         self.client = SlackClient(self.bot_token)
-        # We'll use this dictionary to store the state of each message object.
-        # In a production envrionment you'll likely want to store this more
-        # persistantly in  a database.
+
         self.messages = {}
 
     def auth(self, code):
-        """
-        Authenticate with OAuth and assign correct scopes.
-        Save a dictionary of authed team information in memory on the bot
-        object.
-
-        Parameters
-        ----------
-        code : str
-            temporary authorization code sent by Slack to be exchanged for an
-            OAuth token
-
-        """
-        # After the user has authorized this app for use in their Slack team,
-        # Slack returns a temporary authorization code that we'll exchange for
-        # an OAuth token using the oauth.access endpoint
         auth_response = self.client.api_call(
-                                "oauth.access",
-                                client_id=self.oauth["client_id"],
-                                client_secret=self.oauth["client_secret"],
-                                code=code
-                                )
-        # To keep track of authorized teams and their associated OAuth tokens,
-        # we will save the team ID and bot tokens to the global
-        # authed_teams object
+            "oauth.access",
+            client_id=self.oauth["client_id"],
+            client_secret=self.oauth["client_secret"],
+            code=code
+        )
         team_id = auth_response["team_id"]
         authed_teams[team_id] = {"bot_token":
-                                 auth_response["bot"]["bot_access_token"]}
-        # Then we'll reconnect to the Slack Client with the correct team's
-        # bot token
+                                     auth_response["bot"]["bot_access_token"]}
         self.client = SlackClient(authed_teams[team_id]["bot_token"])
 
-
     def onboarding_message(self, team_id, user_id):
-        """
-        Create and send an onboarding welcome message to new users. Save the
-        time stamp of this message on the message object for updating in the
-        future.
-
-        Parameters
-        ----------
-        team_id : str
-            id of the Slack team associated with the incoming event
-        user_id : str
-            id of the Slack user associated with the incoming event
-
-        """
-        # We've imported a Message class from `message.py` that we can use
-        # to create message objects for each onboarding message we send to a
-        # user. We can use these objects to keep track of the progress each
-        # user on each team has made getting through our onboarding tutorial.
-
-        # First, we'll check to see if there's already messages our bot knows
-        # of for the team id we've got.
         if self.messages.get(team_id):
-            # Then we'll update the message dictionary with a key for the
-            # user id we've recieved and a value of a new message object
             self.messages[team_id].update({user_id: message.Message()})
         else:
-            # If there aren't any message for that team, we'll add a dictionary
-            # of messages for that team id on our Bot's messages attribute
-            # and we'll add the first message object to the dictionary with
-            # the user's id as a key for easy access later.
             self.messages[team_id] = {user_id: message.Message()}
         message_obj = self.messages[team_id][user_id]
-        # Then we'll set that message object's channel attribute to the DM
-        # of the user we'll communicate with
         message_obj.channel = self.open_dm(user_id)
-        # We'll use the message object's method to create the attachments that
-        # we'll want to add to our Slack message. This method will also save
-        # the attachments on the message object which we're accessing in the
-        # API call below through the message object's `attachments` attribute.
         message_obj.create_attachments()
         post_message = self.client.api_call("chat.postMessage",
                                             channel=message_obj.channel,
@@ -117,11 +68,7 @@ class Bot(object):
                                             attachments=message_obj.attachments
                                             )
         timestamp = post_message["ts"]
-        # We'll save the timestamp of the message we've just posted on the
-        # message object which we'll use to update the message after a user
-        # has completed an onboarding task.
         message_obj.timestamp = timestamp
-
 
     def give_kudos(self, emoji, user, channel_id):
         print(f"attempting to give someone kudos from {self}")
