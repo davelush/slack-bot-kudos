c8b5c63f902f377053357289336ae833ca18e05f
davelush <dave.lush@gmail.com>
2019-03-16 08:03:38 +0000

    Cleaning code up a bit

--- kudos/sentiment.py
+++ kudos/sentiment.py
@@ -1,14 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Module for encapsulating the Sentiment class
+"""
 import re
 
 
+#TODO it'd be good it this stored all emojis within a message and provided a score of positive / negative across all of
+# them
+#TODO the set of emojis should be configurable
+
+
 class Sentiment(object):
+    """
+    Sentiment class finds emojis in a message and checks allows checking of whether an emoji is positive or not
+    """
+
     positive_emojis = [":star:",
                        ":star2:",
                        ":stars:",
                        ":cookie:"]
 
     def __init__(self):
-        i = 0
+        pass
 
     def is_positive_emoji(self, emoji):
         if emoji in self.positive_emojis:
@@ -16,10 +29,13 @@ class Sentiment(object):
         else:
             return False
 
-    def contains_emoji(self, text):
+    @staticmethod
+    def contains_emoji(text):
         p = re.compile("[:].*[:]")
         return re.findall(p, text)
 
-    def contains_user(self, text):
+
+    @staticmethod
+    def contains_user(text):
         p = re.compile("[<@].*[>]")
         return re.findall(p, text)
