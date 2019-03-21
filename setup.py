5464375577565acf5f5c5855d6eb42c0d459a7a2
davelush <dave.lush@gmail.com>
2019-03-21 08:08:43 +0000

    Think we are fully pipenv now

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+non_test_packages = [p for p in find_packages() if p.split('.')[-1] != 'tests']
+requirements = []
+
 setup(
-    name="SlackBotSandbox",
-    version="0.0.1-snapshot",
+    name="slack-bot-kudos",
+    version="0.1",
     license='Apache License 2.0',
-    packages=find_packages(),
-    package_dir={'slack_bot': 'slack_bot', 'kudos': 'kudos'},
+    packages=non_test_packages,
     long_description=open('README.md').read(),
     test_suite='tests',
     setup_requires=['pytest-runner'],
-    install_requires=['PyYAML']
+    install_requires=requirements
 )
