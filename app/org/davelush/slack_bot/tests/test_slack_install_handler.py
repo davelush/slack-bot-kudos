93cebcb2e3ecc744a8132ef1332171f38a3c01d0
davelush <dave.lush@gmail.com>
2019-05-02 20:54:35 +0100

    [#4] Removing the message class and adding a first test on the install handler

---
+++ app/org/davelush/slack_bot/tests/test_slack_install_handler.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from unittest.mock import patch, Mock
+
+from org.davelush.slack_bot.slack_install_handler import SlackInstallHandler
+
+
+class TestSlackInstallHandler(TestCase):
+
+    def test_get_happy(self):
+        with patch("org.davelush.slack_bot.bot.Bot") as mock_slack_bot:
+            with patch("org.davelush.slack_bot.slack_install_handler.render_template") as mock_render:
+                mock_slack_bot.oauth = {"client_id": "id", "scope": "scope"}
+                slack_install_handler = SlackInstallHandler(**{"py_bot": mock_slack_bot})
+                result = slack_install_handler.get()
+                self.assertIsNotNone(result)
