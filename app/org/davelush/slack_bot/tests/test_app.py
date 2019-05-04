2672233712642c64398ce7194425ab1dbcee3473
davelush <dave.lush@gmail.com>
2019-05-04 06:23:03 +0100

    Swapped out a bucnh of assertions for proper TestCase ones

--- app/org/davelush/slack_bot/tests/test_app.py
+++ app/org/davelush/slack_bot/tests/test_app.py
@@ -14,25 +14,25 @@ class TestApp(TestCase):
 
     def test_config_defaults(self):
         args = parse_cli_args(sys.argv[1:], os.environ)
-        assert args.postgres_host == "localhost"
-        assert args.postgres_port == 5432
-        assert args.postgres_db == "metrics"
-        assert args.postgres_schema == "kudosbot"
-        assert args.client_id == "id"
-        assert args.client_secret == "secret"
-        assert args.bot_token == "bot_token"
-        assert args.postgres_user is None
-        assert args.postgres_pass is None
+        self.assertEqual(args.postgres_host, "localhost")
+        self.assertEqual(args.postgres_port, 5432)
+        self.assertEqual(args.postgres_db, "metrics")
+        self.assertEqual(args.postgres_schema, "kudosbot")
+        self.assertEqual(args.client_id, "id")
+        self.assertEqual(args.client_secret, "secret")
+        self.assertEqual(args.bot_token, "bot_token")
+        self.assertIsNone(args.postgres_user)
+        self.assertIsNone(args.postgres_pass)
 
     def test_config_env_vars(self):
         os.environ["POSTGRES_HOST"] = "some.random.host"
         args = parse_cli_args(sys.argv[1:], os.environ)
-        assert args.postgres_host == "some.random.host"
+        self.assertEquals(args.postgres_host, "some.random.host")
 
     def test_setup(self):
         with patch("psycopg2.connect") as mock_psycopg2_connect:
             mock_psycopg2_connect.return_value = None
             flask_app, flask_api = setup()
-            assert flask_app is not None
-            assert flask_api is not None
-            assert len(flask_api.endpoints) > 0
+            self.assertIsNotNone(flask_app)
+            self.assertIsNotNone(flask_api)
+            self.assertTrue(len(flask_api.endpoints) > 0)
