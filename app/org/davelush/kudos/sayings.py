98f20fb5a8895d4bfc452c160d7436b65253d19c
davelush <dave.lush@gmail.com>
2019-05-20 21:10:55 +0100

    Missed a comma... And adding one to the last line

--- app/org/davelush/kudos/sayings.py
+++ app/org/davelush/kudos/sayings.py
@@ -12,11 +12,11 @@ class Sayings(object):
                          f"Who has {amount} kudos {user}? You do... You have {amount} kudos :star:",
                          f":trophy: :trophy: :chicken: :knife_fork_plate: Winner winner chicken dinner :trophy: :trophy: :chicken: :knife_fork_plate: {user} has {amount} kudos",
                          f":dart: That's band on target {user} :dart: You now have {amount} kudos",
-                         f":fishing_pole_and_fish: Have you been fishing for compliments {user}? :fishing_pole_and_fish: Well it worked! You have {amount} kudos"
+                         f":fishing_pole_and_fish: Have you been fishing for compliments {user}? :fishing_pole_and_fish: Well it worked! You have {amount} kudos",
                          f":church: :raised_hands: All praise {user} :raised_hands: :church: You have {amount} kudos",
                          f":hypnotoad: Hypnotoad says `all praise` {user}... `Give them more kudos` :hypnotoad: You now have {amount} kudos",
                          f"You're in pole position {user} :racing_car::racing_car::racing_car::racing_car::racing_car: You have {amount} kudos",
-                         f":gem: :gem: You little diamond {user} :gem: :gem: You now have {amount} kudos"
+                         f":gem: :gem: You little diamond {user} :gem: :gem: You now have {amount} kudos",
                          ]
         return random.choice(kudos_sayings)
 
