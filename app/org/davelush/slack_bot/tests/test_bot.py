43d5cbc124d44b3b9b23b5dfa4884cea933bb3d9
davelush <dave.lush@gmail.com>
2019-05-21 21:03:22 +0100

    [#4] Additional pytest coverage

--- app/org/davelush/slack_bot/tests/test_bot.py
+++ app/org/davelush/slack_bot/tests/test_bot.py
@@ -3,6 +3,8 @@ from unittest.mock import patch
 
 from org.davelush.slack_bot.bot import Bot
 
+overall_query_result = [["some_id_1", 3], ["some_id_2", 2]]
+monthly_query_result = [["some_id_3", 5], ["some_id_4", 1]]
 
 class TestBot(TestCase):
 
@@ -23,3 +25,20 @@ class TestBot(TestCase):
                 mock_slack_client.api_call.return_value = "OK"
                 result = bot.give_kudos("user", 1, "channel", "text", "message_id", "event_id")
                 self.assertFalse(result)
+
+    def test_get_leaderboard(self):
+        with patch("slackclient.client.SlackClient") as mock_slack_client:
+            with patch("psycopg2.connect") as mock_conn:
+                bot = Bot(mock_conn, mock_slack_client, "", "")
+                mock_conn.cursor.return_value.__iter__.return_value = overall_query_result
+                result = bot.get_leaderboard()
+                self.assertEquals(result, ':rocket: *Kudos Leaderboard* :rocket:\n1. some_id_1 has 3 kudos\n2. some_id_2 has 2 kudos\n')
+
+
+    def test_get_stats(self):
+        with patch("slackclient.client.SlackClient") as mock_slack_client:
+            with patch("psycopg2.connect") as mock_conn:
+                bot = Bot(mock_conn, mock_slack_client, "", "")
+                mock_conn.cursor.return_value.__iter__.return_value = monthly_query_result
+                result = bot.get_stats(2019, 5)
+                self.assertEquals(result, ':rocket: *Kudos Recipients for 2019/5* :rocket:\n1. some_id_3 has 5 kudos\n2. some_id_4 has 1 kudos\n')
