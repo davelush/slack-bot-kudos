4acb6d033fec9c7264a335d70f1593b058eb0729
davelush <dave.lush@gmail.com>
2020-10-22 20:26:04 +0100

    Restructuring application code to starting bringing lambda and original flask app together

--- handler.py
+++ handler.py
@@ -4,7 +4,7 @@ import json
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "event": event
     }
 
     response = {
